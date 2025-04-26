1169f6e711aa5d9e452255034e0c6a7e7a90a15a
DanaF <x.dfortier@gmail.com>
2025-04-25 22:31:50 -0700

    rotate player direction to move relative to cam wip

--- script/Player.cs
+++ script/Player.cs
@@ -11,6 +11,19 @@ public partial class Player : CharacterBody3D
     private float _minPitch = -90f;
     [Export]
     private float _maxPitch = 90f;
+    
+    [Export]
+    private int _fallAcceleration { get; set; } = 0;
+    [Export] 
+    private int _speed { get; set; } = 10;
+    
+    private Vector3 _targetVelocity = Vector3.Zero;
+
+    private float _cameraRotationY = 0f;
+    private float _cameraRotationX = 0f;
+    private Vector3 _rotatedDirection = Vector3.Zero;
+    
+    
 
     private float _yaw = 0f;
     private float _pitch = 0f;
@@ -22,11 +35,45 @@ public partial class Player : CharacterBody3D
         camera = GetNode<Camera3D>("Camera3D");
     }
 
+    public override void _PhysicsProcess(double delta)
+    {
+        var direction = Vector3.Zero;
+        var cameraYRotation = camera.Rotation.Y;
+        if (Input.IsActionPressed("move_right"))
+            direction.X += 1.0f;
+        if (Input.IsActionPressed("move_left"))
+            direction.X -= 1.0f;
+        if (Input.IsActionPressed("move_back"))
+            direction.Z += 1.0f;
+        if (Input.IsActionPressed("move_forward"))
+            direction.Z -= 1.0f;
+
+        if (direction != Vector3.Zero)
+            direction = direction.Normalized();
+
+        // If you want movement relative to camera, rotate direction by camera's Y rotation here
+
+        _cameraRotationY = camera.Rotation.Y;
+        _cameraRotationX = camera.Rotation.X;
+        _rotatedDirection = direction.X.Rotated(Vector3.Up, _cameraRotationY);
+        _targetVelocity.X = direction.X * _speed;
+        _targetVelocity.Z = direction.Z * _speed;
+
+        // Apply gravity
+        if (!IsOnFloor())
+            _targetVelocity.Y -= _fallAcceleration * (float)delta;
+        else
+            _targetVelocity.Y = 0;
+
+        Velocity = _targetVelocity;
+        MoveAndSlide();
+    }
+
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        
-        var mouseMotion = @event as InputEventMouseMotion;
+
+       var mouseMotion = @event as InputEventMouseMotion;
         if (mouseMotion == null) return;
       
         GD.Print(mouseMotion.Relative);
