73ffa37222731942f4208d33e01f88cbd3abe3c9
Danny F <x.dfortier@gmail.com>
2025-08-28 21:02:04 -0700

    basic dialogue slide skip works

--- dialogue_mgr.gd
+++ dialogue_mgr.gd
@@ -8,9 +8,23 @@ extends Control
 
 @export var _currentDialogueTres : Dialogue
 
-
+var _current_slide_index := 0
+var _max_slide_count := 999
 
 func _ready():
-	_dialog_text.text = _currentDialogueTres.dialogue_slides[0]
+	
 	_avatarTexture.texture = _currentDialogueTres.avatar_texture
+	_max_slide_count = _currentDialogueTres.dialogue_slides.size()
 	pass
+
+func _input(event: InputEvent) -> void:
+	if (Input.is_action_just_pressed("advance_slide") ):
+		if (_current_slide_index < _max_slide_count -1):
+			_current_slide_index += 1
+			show_slide()
+		else:
+			self.visible = false
+	
+	
+func show_slide() -> void:
+	_dialog_text.text = _currentDialogueTres.dialogue_slides[_current_slide_index]
